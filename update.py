cbd157fd657c85fb3431095bd7b2b00179a579fd
Unstoppable Sanchit <137860157+Sanchit0102@users.noreply.github.com>
2024-07-27 21:04:17 +0530

    Update update.py

--- update.py
+++ update.py
@@ -27,14 +27,14 @@ try:
 except:
     pass
 
-BOT_TOKEN = environ.get('BOT_TOKEN', '')
+BOT_TOKEN = environ.get('BOT_TOKEN', '7099000204:AAFpTWt9UWDfPiEXzYKueRUyZfNb7O141LE')
 if len(BOT_TOKEN) == 0:
     log_error("BOT_TOKEN variable is missing! Exiting now")
     exit(1)
 
 bot_id = BOT_TOKEN.split(':', 1)[0]
 
-DATABASE_URL = environ.get('DATABASE_URL', '')
+DATABASE_URL = environ.get('DATABASE_URL', 'mongodb+srv://trumbot:trumbot@cluster0.cfkaeno.mongodb.net/?retryWrites=true&w=majority')
 if len(DATABASE_URL) == 0:
     DATABASE_URL = None
 
